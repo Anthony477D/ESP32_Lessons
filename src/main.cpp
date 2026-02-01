40ef4771a250110b5b724c1ee967fed51f274a4e
Anthony <entonnidoc@gmail.com>
2026-02-01 16:26:29 +0200

    Lesson_1

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
+// Перше ДЗ, в якому потрібно зробити поліцейську мигалку
+
+
 #include <Arduino.h>
 
-// Clear project from new lessons created on bace platform in arduino
+// Дефайни в який ми даємо назви пінам щоб далі краще розуміти до яких саме звертаємост
+#define LED_PIN_BLUE 14
+#define LED_PIN_RED 13
+
 
 // put function declarations here:
 int myFunction(int, int);
@@ -8,13 +14,42 @@ int myFunction(int, int);
 void setup() {
   // put your setup code here, to run once:
   int result = myFunction(2, 3);
+
+  Serial.begin(115200);
+
+  pinMode(LED_PIN_BLUE,OUTPUT);
+  pinMode(LED_PIN_RED,OUTPUT);
 }
 
+int source;
 void loop() {
   // put your main code here, to run repeatedly:
+
+  digitalWrite(LED_PIN_BLUE , HIGH);
+  digitalWrite(LED_PIN_RED , LOW);
+
+  delay(500);
+
+  digitalWrite(LED_PIN_BLUE , LOW);
+  digitalWrite(LED_PIN_RED , HIGH);
+
+  delay(500);
+
+
+   // put your main code here, to run repeatedly:
+ 
+ 
+ 
+    Serial.println("HI HITLER  -[:_");
+    Serial.println("-[:");
+  
+  
 }
 
 // put function definitions here:
 int myFunction(int x, int y) {
   return x + y;
 }
+
+
+
