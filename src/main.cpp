eee1134ad60e74ce1de6f71c7ebe39ab0fa38373
Anthony <entonnidoc@gmail.com>
2026-02-28 15:38:29 +0200

    Finish

--- src/main.cpp
+++ src/main.cpp
@@ -6,21 +6,96 @@
 
 */
 
+// створюємо дкфайни для пінів які ми використовуємо
+#define LED_PIN 14 
+#define BUTTON_PIN 3
+
+#define USER_DELAY 500
 
 
-// put function declarations here:
-int myFunction(int, int);
+// створюємо клас перерахування статусів в яких може знаходитись наш світлодіод
+/*
+enum можна використовувати і без class, але такий метод дозволяж створити декілька перерахувань з одними змінними
+якщо без class то область видимості змінних в перерахуванні глобальна
+*/
+enum class LedState {
+  On,
+  Off
+};
+
+/*
+Створюємо клас Config в якому зберігаються конфігурацційні змінні 
+*/
+class Config{
+  public:
+    static constexpr uint8_t LED = LED_PIN;
+    static constexpr uint8_t BUTTON = BUTTON_PIN;
+    static constexpr uint16_t BLINK_DELAY = USER_DELAY;
+};
+
+
+/*
+створюємо клас Led, по образу і подобі. якого будуть створювати елементи класу
+*/
+class Led{
+  // вказуємо приватні змінні, область видимості яких буде лоступний лише для обєкту
+  private:
+    uint8_t _pin;
+  
+  // робимо параметер класу Led публічний для того щоб вносити данні ззовні
+  public:
+    Led(uint8_t pin_number_OutClass){
+      _pin = pin_number_OutClass; // присвоємо зовніній параметер внутріщній змінні
+    }
+     
+  // робимо функцію ініціалізації для піна мікроконтроллера
+  void init() { 
+    pinMode(_pin,OUTPUT);
+  }
+
+  // робимо функціб для налаштування обєкту класу
+  // в параметрах функції вказуємо обєет enum з якого буде взята змінна, та називоємо її state для подільної роботи з нею
+  // в порівняннях, вказуємо фіксоване значення LedState, та те що ми отримуємо коли викликаємо функцію обєкта
+  void set(LedState state){ 
+    if(state == LedState::On){
+      digitalWrite(_pin, HIGH);
+    } 
+    if(state == LedState::Off){
+      digitalWrite(_pin, LOW);
+    }
+  }
+};
+
+// створюємо обєкт MyLed класу Led
+Led MyLed(Config::LED);
+
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  MyLed.init();
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+  while(digitalRead(Config::BUTTON) == HIGH ){
+    MyLed.set(LedState::On);
+    delay(Config::BLINK_DELAY);
+
+    MyLed.set(LedState::Off);
+    delay(Config::BLINK_DELAY);
+  } 
+
+  MyLed.set(LedState::Off);
+
+
+  /* тест роботи світлодіода напряму 
+  digitalWrite(LED_PIN , LOW);
+  delay(500);
+
+  digitalWrite(LED_PIN , HIGH);
+  delay(500);
+
+  */
+  
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
