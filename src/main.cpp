68e71a760d7bfec6a57003e6195595f07c14e629
Anthony <entonnidoc@gmail.com>
2026-02-28 18:12:13 +0200

    unrealLAst

--- src/main.cpp
+++ src/main.cpp
@@ -10,9 +10,9 @@
 #define LED_PIN 14 
 #define BUTTON_PIN 3
 
-#define USER_DELAY 500
+#define USER_DELAY 150
 
-
+bool UserDelay(uint32_t time);
 // створюємо клас перерахування статусів в яких може знаходитись наш світлодіод
 /*
 enum можна використовувати і без class, але такий метод дозволяж створити декілька перерахувань з одними змінними
@@ -22,6 +22,14 @@ enum class LedState {
   On,
   Off
 };
+
+enum SystemState{
+  LedOn,
+  LedOff,
+  LedNull
+};
+
+SystemState SystemStateNow = LedNull;
 
 /*
 Створюємо клас Config в якому зберігаються конфігурацційні змінні 
@@ -72,30 +80,62 @@ Led MyLed(Config::LED);
 
 void setup() {
   MyLed.init();
+  pinMode(Config::BUTTON, INPUT);
 
+  Serial.begin(115200); // Додаємо монітор порту для діагностики
+  Serial.println("System Started");
 }
 
+uint32_t start = 0; // змінна для відліку
+bool BT = false; // змінна для запису положення кнопки
+bool LastLedState = false; // змінна для запису останнього стану світлодіода
+
 void loop() {
+  
 
-  while(digitalRead(Config::BUTTON) == HIGH ){
-    MyLed.set(LedState::On);
-    delay(Config::BLINK_DELAY);
+  // опитуємо статус кнопки
+  BT = digitalRead(Config::BUTTON);
 
+  // якщо кнопка відпущена світлодіод вимикаємо
+  // Вказуємо статус світлодіода як Null
+  if(BT == false){
     MyLed.set(LedState::Off);
-    delay(Config::BLINK_DELAY);
-  } 
+    SystemStateNow = LedNull;
+  }  
+  
+  if(BT == true){
 
-  MyLed.set(LedState::Off);
+    // якшо кнопка стала натиснутою вказуємо статус ситеми що кнопка натиснкта, та становлюємо початок відоіку
+    if(SystemStateNow == LedNull){
+      SystemStateNow = LedOff;
+      start = millis();
+      
+    }
+    
+    if(SystemStateNow == LedOff && (millis()-start >= Config::BLINK_DELAY)){
+      MyLed.set(LedState::On);
+      SystemStateNow = LedOn;
+      start = millis();
+      
+    }
+    
+    if(SystemStateNow == LedOn && (millis() - start >= Config::BLINK_DELAY)){
+      MyLed.set(LedState::Off); 
+      SystemStateNow = LedOff;
+      start = millis();
+      
+    }
+    /*
+    while(digitalRead(Config::BUTTON) == HIGH ){
+      //MyLed.set(LedState::On);
+      //delay(Config::BLINK_DELAY);
 
-
-  /* тест роботи світлодіода напряму 
-  digitalWrite(LED_PIN , LOW);
-  delay(500);
-
-  digitalWrite(LED_PIN , HIGH);
-  delay(500);
-
-  */
+   
+      //MyLed.set(LedState::Off);
+     //delay(Config::BLINK_DELAY);
+    }
+    */
+  }
   
 }
 
